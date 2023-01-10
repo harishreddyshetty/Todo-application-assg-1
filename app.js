45b9a3fa9b1166feccc37e7abbd4c91dd880bae8
harish632 <harish.redyshetty@gmail.com>
2023-01-10 11:21:01 +0000

    assignment 1

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ const hasCategoryAndHasPriority = (requestQuery) =>
 
 const hasTodo = (requestQuery) => requestQuery.todo !== undefined;
 
+const hasSearch = (requestQuery) => requestQuery.search_q !== undefined;
+
 // api 1
 const convertToCamelCase = (object) => {
   return {
@@ -56,10 +58,10 @@ const convertToCamelCase = (object) => {
 };
 
 app.get("/todos/", async (request, response) => {
-  const { status } = request.query;
-  console.log(status);
+  const { status, priority, search_q = "", category } = request.query;
 
   switch (true) {
+    // scenario 1
     case hasStatus(request.query):
       if (status === "TO DO" || status === "IN PROGRESS" || status === "DONE") {
         const getTodosQuery = `SELECT * FROM todo WHERE status = "${status}"`;
@@ -70,6 +72,140 @@ app.get("/todos/", async (request, response) => {
         response.send("Invalid Todo Status");
       }
       break;
+    // scenario 2
+    case hasPriority(request.query):
+      if (priority === "HIGH" || priority === "MEDIUM" || priority === "LOW") {
+        const getTodosQuery = `SELECT * FROM todo WHERE priority = "${priority}"`;
+        const data = await db.all(getTodosQuery);
+        response.send(data.map((eachItem) => convertToCamelCase(eachItem)));
+      } else {
+        response.status(400);
+        response.send("Invalid Todo Priority");
+      }
+      break;
+    // scenario 3
+    case hasPriorityAndHasStatus(request.query):
+      const getTodosQuery = `SELECT * FROM todo WHERE status = "${status}" and priority = "${priority}"`;
+      const data = await db.all(getTodosQuery);
+      response.send(data.map((eachItem) => convertToCamelCase(eachItem)));
+      break;
+
+    // scenario 4
+    case hasSearch(request.query):
+      const Query = `SELECT * FROM todo WHERE todo LIKE "%${search_q}%"`;
+      const searchData = await db.all(Query);
+      response.send(searchData.map((eachItem) => convertToCamelCase(eachItem)));
+      break;
+
+    // scenario 5
+    case hasCategoryAndHasStatus(request.query):
+      const CateAndStatQuery = `SELECT * FROM todo WHERE status = "${status}" and category = "${category}"`;
+      const catStatusResponse = await db.all(CateAndStatQuery);
+      response.send(
+        catStatusResponse.map((eachItem) => convertToCamelCase(eachItem))
+      );
+      break;
+
+    // scenario 6
+    case hasCategory(request.query):
+      if (
+        category === "WORK" ||
+        category === "HOME" ||
+        category === "LEARNING"
+      ) {
+        const getCategoryQuery = `SELECT * FROM todo WHERE  category = "${category}"`;
+        const categoryResponse = await db.all(getCategoryQuery);
+        response.send(
+          categoryResponse.map((eachItem) => convertToCamelCase(eachItem))
+        );
+      } else {
+        response.status(400);
+        response.send("Invalid Todo Category");
+      }
+      break;
+
+    //scenario 7
+    case hasCategoryAndHasPriority(request.query):
+      const catAndPriorityQuery = `SELECT * FROM todo WHERE  category = "${category}" and priority = "${priority}"`;
+      const catAndPriorityResponse = await db.all(catAndPriorityQuery);
+      response.send(
+        catAndPriorityResponse.map((eachItem) => convertToCamelCase(eachItem))
+      );
+      break;
+  }
+});
+
+// api 2
+
+app.get("/todos/:todoId/", async (request, response) => {
+  const { todoId } = request.params;
+  const query = `SELECT * FROM todo WHERE id = ${todoId};`;
+  const dbResponse = await db.get(query);
+  response.send(convertToCamelCase(dbResponse));
+});
+
+//api 3
+
+//api 4
+
+const isStatusValid = (requestQuery) => {
+  const { status } = requestQuery;
+  let result;
+  if (status === "TO DO" || status === "IN PROGRESS" || status === "DONE") {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+};
+const isPriorityValid = (requestQuery) => {
+  const { priority } = requestQuery;
+  let result;
+  if (priority === "HIGH" || priority === "MEDIUM" || priority === "LOW") {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+const isCategoryValid = (requestQuery) => {
+  const { category } = requestQuery;
+  let result;
+  if (category === "WORK" || category === "HOME" || category === "LEARNING") {
+    result = true;
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+app.post("/todos/", async (request, response) => {
+  const { id, todo, priority, status, category, dueDate } = request.body;
+  const isStatus = isStatusValid(request.body);
+  const isPriority = isPriorityValid(request.body);
+  const isCategory = isCategoryValid(request.body);
+
+  if (isStatus && isPriority && isCategory) {
+    const addTodoQuery = `INSERT INTO todo(id,todo,priority,status,category,due_date)
+    VALUES(
+        ${id},
+        "${todo}",
+        "${priority}",
+        "${status}",
+        "${category}",
+        "${dueDate}");`;
+    await db.run(addTodoQuery);
+    response.send("Todo Successfully Added");
+  } else if (!isStatus) {
+    response.status(400);
+    response.send("Invalid Todo Status");
+  } else if (!isPriority) {
+    response.status(400);
+    response.send("Invalid Todo Priority");
+  } else if (!isCategory) {
+    response.status(400);
+    response.send("Invalid Todo Category");
   }
 });
 
